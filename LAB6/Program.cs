36c6bfa149e6c12ff44e2a22e355b9542db4a354
Andriymatvienko <130711488+Andriymatvienko@users.noreply.github.com>
2023-11-18 21:25:19 +0200

    Add files via upload

---
+++ LAB6/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+class TCIRCLE<T>
+{
+    public T radius;
+    public Tuple<T, T> center;
+
+    public TCIRCLE()
+    {
+        radius = default(T);
+        center = new Tuple<T, T>(default(T), default(T));
+    }
+
+    public TCIRCLE(T radius, Tuple<T, T> center)
+    {
+        this.radius = radius;
+        this.center = center;
+    }
+
+    // Конструктор копіювання
+    public TCIRCLE(TCIRCLE<T> circle)
+    {
+        this.radius = circle.radius;
+        this.center = circle.center;
+    }
+
+    // Введення даних
+    public void InputData()
+    {
+        Console.Write("Введіть радіус круга: ");
+        radius = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+        Console.Write("Введіть координату X центру кола: ");
+        T x = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+        Console.Write("Введіть координату Y центру кола: ");
+        T y = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+        center = new Tuple<T, T>(x, y);
+    }
+
+    // Виведення даних
+    public void Display()
+    {
+        Console.WriteLine($"Радіус: {radius}, Центр: {center}");
+    }
+
+    // Перевірка належності точки колу
+    public bool IsPointInside(Tuple<T, T> point)
+    {
+        double distance = Math.Sqrt(Math.Pow(Convert.ToDouble(point.Item1) - Convert.ToDouble(center.Item1), 2) + Math.Pow(Convert.ToDouble(point.Item2) - Convert.ToDouble(center.Item2), 2));
+        return distance <= Convert.ToDouble(radius);
+    }
+
+    // Перевантаження операторів
+    public static T operator +(TCIRCLE<T> circle1, TCIRCLE<T> circle2)
+    {
+        return (T)Convert.ChangeType(Convert.ToDouble(circle1.radius) + Convert.ToDouble(circle2.radius), typeof(T));
+    }
+
+    public static T operator -(TCIRCLE<T> circle1, TCIRCLE<T> circle2)
+    {
+        return (T)Convert.ChangeType(Convert.ToDouble(circle1.radius) - Convert.ToDouble(circle2.radius), typeof(T));
+    }
+
+    public static T operator *(TCIRCLE<T> circle, double num)
+    {
+        return (T)Convert.ChangeType(Convert.ToDouble(circle.radius) * num, typeof(T));
+    }
+}
+
+class TBALL<T> : TCIRCLE<T>
+{
+    public TBALL(T radius, Tuple<T, T> center) : base(radius, center) { }
+
+    public double Volume()
+    {
+        return (4 / 3) * 3.14159 * (Math.Pow(Convert.ToDouble(radius), 3));
+    }
+}
+
+// Програма клієнт для тестування
+class Program
+{
+    static void Main()
+    {
+        TCIRCLE<double> circle = new TCIRCLE<double>(5, new Tuple<double, double>(0, 0));
+        circle.Display(); // Виведення даних
+        Console.WriteLine("Точка (1, 2) належить кругу: " + circle.IsPointInside(new Tuple<double, double>(1, 2))); // Перевірка належності точки колу
+        Console.WriteLine("Додавання радіусів: " + (circle + new TCIRCLE<double>(3, new Tuple<double, double>(0, 0)))); // Перевантаження оператора +
+        Console.WriteLine("Множення радіуса на число: " + circle * 2); // Перевантаження оператора *
+
+        TBALL<double> ball = new TBALL<double>(5, new Tuple<double, double>(0, 0));
+        Console.WriteLine("Об'єм кулі: " + ball.Volume()); // Метод для знаходження об'єму кулі
+    }
+}
---
+++ Lab6_2/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+class TSMATRIX<T>
+{
+    public T[,] matrix;
+    public int rows;
+    public int columns;
+
+    public TSMATRIX()
+    {
+        rows = 0;
+        columns = 0;
+        matrix = new T[rows, columns];
+    }
+
+    public TSMATRIX(T[,] elements)
+    {
+        rows = elements.GetLength(0);
+        columns = elements.GetLength(1);
+        matrix = elements;
+    }
+
+    public TSMATRIX(TSMATRIX<T> other)
+    {
+        rows = other.rows;
+        columns = other.columns;
+        matrix = other.matrix;
+    }
+
+    public void InputData()
+    {
+        Console.WriteLine("Введiть розмiрнiсть матрицi:");
+        Console.Write("Рядки: ");
+        rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Стовпцi: ");
+        columns = Convert.ToInt32(Console.ReadLine());
+
+        matrix = new T[rows, columns];
+
+        Console.WriteLine("Введіть елементи матрицi:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Елемент [{i},{j}]: ");
+                matrix[i, j] = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            }
+        }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Матриця:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public T FindMaxElement()
+    {
+        T max = matrix[0, 0];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (Comparer<T>.Default.Compare(matrix[i, j], max) > 0)
+                {
+                    max = matrix[i, j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public T FindMinElement()
+    {
+        T min = matrix[0, 0];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (Comparer<T>.Default.Compare(matrix[i, j], min) < 0)
+                {
+                    min = matrix[i, j];
+                }
+            }
+        }
+        return min;
+    }
+
+    public dynamic CalculateSumOfElements()
+    {
+        dynamic sum = default(T);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                sum += matrix[i, j];
+            }
+        }
+        return sum;
+    }
+
+    public static TSMATRIX<T> operator +(TSMATRIX<T> matrix1, TSMATRIX<T> matrix2)
+    {
+        T[,] result = new T[matrix1.rows, matrix1.columns];
+
+        for (int i = 0; i < matrix1.rows; i++)
+        {
+            for (int j = 0; j < matrix1.columns; j++)
+            {
+                dynamic value1 = matrix1.matrix[i, j];
+                dynamic value2 = matrix2.matrix[i, j];
+                result[i, j] = value1 + value2;
+            }
+        }
+
+        return new TSMATRIX<T>(result);
+    }
+
+    public static TSMATRIX<T> operator -(TSMATRIX<T> matrix1, TSMATRIX<T> matrix2)
+    {
+        T[,] result = new T[matrix1.rows, matrix1.columns];
+
+        for (int i = 0; i < matrix1.rows; i++)
+        {
+            for (int j = 0; j < matrix1.columns; j++)
+            {
+                dynamic value1 = matrix1.matrix[i, j];
+                dynamic value2 = matrix2.matrix[i, j];
+                result[i, j] = value1 - value2;
+            }
+        }
+
+        return new TSMATRIX<T>(result);
+    }
+}
+
+class TDeterminant2 : TSMATRIX<int>
+{
+    public TDeterminant2(int[,] elements) : base(elements) { }
+
+    public int CalculateDeterminant()
+    {
+        if (rows != 2 || columns != 2)
+        {
+            Console.WriteLine("Визначник можна знайти лиш для  2x2 матрицi.");
+            return 0;
+        }
+
+        return (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TSMATRIX<int> matrix1 = new TSMATRIX<int>();
+        matrix1.InputData();
+        matrix1.Display();
+
+        Console.WriteLine($"Max element: {matrix1.FindMaxElement()}");
+        Console.WriteLine($"Min element: {matrix1.FindMinElement()}");
+        Console.WriteLine($"Sum of elements: {matrix1.CalculateSumOfElements()}");
+
+        TSMATRIX<int> matrix2 = new TSMATRIX<int>();
+        matrix2.InputData();
+        matrix2.Display();
+
+        TSMATRIX<int> sum = matrix1 + matrix2;
+        Console.WriteLine("Сума:");
+        sum.Display();
+
+        TSMATRIX<int> difference = matrix1 - matrix2;
+        Console.WriteLine("Рiзниця:");
+        difference.Display();
+
+        TDeterminant2 determinant = new TDeterminant2(new int[,] { { 2, 3 }, { 1, 4 } });
+        int result = determinant.CalculateDeterminant();
+        Console.WriteLine($"Визначник: {result}");
+    }
+}
